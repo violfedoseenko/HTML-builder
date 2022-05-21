6acfbfc89897bb4628f030b71c66af11a31302a4
Violetta <viol.fedoseenko@yandex.ru>
2022-05-21 11:48:56 +0300

    feat: second task is compleated

---
+++ 02-write-file/index.js
@@ -0,0 +1,20 @@
+let path = require('path');
+let fs = require('fs');
+let { stdout, stdin, exit } = require('process');
+
+let writeStream = fs.createWriteStream(path.join(__dirname, 'my_text.txt'));
+
+stdout.write('Введите текст:');
+
+stdin.on('data', (data) => {
+  if (data.toString() === 'exit') {
+    finishFunk();
+  }
+  writeStream.write(data);
+});
+
+process.on('SIGINT', finishFunk);
+function finishFunk() {
+  stdout.write('Запись добавлена в файл. До встречи!');
+  exit();
+}
