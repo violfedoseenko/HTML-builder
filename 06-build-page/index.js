960148359e082682c17e7c28ffc8a8d60766efbf
Violetta <viol.fedoseenko@yandex.ru>
2022-05-26 14:09:47 +0300

    refactor sixth task

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,7 +1,8 @@
 let path = require('path');
 let fs = require('fs');
-let fsPromise = require('fs/promises');
+const fsPromise = require('fs/promises');
 
+//коприрование папки assets cо всем содержимым
 let mainDir = path.join(__dirname, 'assets');
 let target = path.join(__dirname, 'project-dist');
 let targetDir = path.join(__dirname, 'project-dist/assets');
@@ -10,54 +11,34 @@ let folderStyles = path.join(__dirname, 'styles');
 let bundle = path.join(__dirname, 'project-dist', 'style.css');
 
 
-//создание директорий
+async function copyFloader(mainDir, targetDir) { 
+  await fsPromise.rm(targetDir, { recursive: true, force: true });
+  await fsPromise.mkdir(targetDir, { recursive: true });
+  let files = await fsPromise.readdir(mainDir, { withFileTypes: true}); 
+  files.forEach(function(file) {
+    let mainFileDir = path.join(mainDir, file.name);
+    let copyFileDir = path.join(targetDir, file.name);
+    if(file.isDirectory()) {
+      copyFloader(mainFileDir, copyFileDir);
+    } else {
+      fsPromise.copyFile(mainFileDir, copyFileDir);
 
-fs.mkdir((path.join(__dirname, 'project-dist')), err => {
-  if(err) throw err;
-});
-fs.mkdir((path.join(__dirname, 'project-dist', 'assets')), err => {
-  if(err) throw err;
-});
-
-
-
-
-
-function copyFloader(mainDir, targetDir) { fsPromise.rm(targetDir, {recursive: true, force: true}).finally(function() {
-  fsPromise.mkdir(targetDir, {
-    recursive: true
+    }   
   });
-  fsPromise.readdir(mainDir, { withFileTypes: true}).then(function(files) {
-    files.forEach(function(file) {
-      let mainFileDir = path.join(mainDir, file.name);
-      let copyFileDir = path.join(targetDir, file.name);
-      if(file.isDirectory()) {
-        copyFloader(mainFileDir,copyFileDir);
-      } else {
-        fsPromise.copyFile(mainFileDir,copyFileDir);
-      }   
-    });
-    console.log('Папка скопирована!');
-  });
-});
+  console.log('Папка скопирована!');
 }
-copyFloader(mainDir, targetDir);
 
 //сбор стилей в один файл
 
-function copyCss() { fs.readdir(folderStyles, 'utf-8', function (error, files) {
+async function copyCss() { fs.readdir(folderStyles, 'utf-8', function (error, files) {
   if (error)
   {throw error;}
   //data = '' - обнуляем файл, на случай если стили в исходниказх изменились
   fs.writeFile(bundle, '', function (error) {
-    if (error) {
-      throw error;
-    }
+    if (error) { throw error;}
   });
   files.forEach(function (file) {
-    console.log('зашли!');
     if ( path.parse(path.join(folderStyles, file)).ext === '.css' ) {
-
       let currentFile = fs.createReadStream(path.join(folderStyles, file));
       currentFile.on('data', function(data) {
         fs.appendFile(bundle, data, function(error) {
@@ -69,10 +50,8 @@ function copyCss() { fs.readdir(folderStyles, 'utf-8', function (error, files) {
   });
 });
 }
-copyCss();
 
-
-function replaceTemplateTags() {
+async function replaceTemplateTags() {
   fs.copyFile(`${__dirname}\\template.html`, `${target}\\index.html`, function (error) {
     if (error) throw error;
     fs.readFile(`${target}\\index.html`, 'utf8', function(error, data) {
@@ -84,6 +63,7 @@ function replaceTemplateTags() {
             if(error) throw error;
             let tagName = `{{${file.name.split('.')[0]}}}`;
             data = data.replace(tagName, dataFile);
+            console.log('перезапись');
             fs.writeFile(`${target}\\index.html`, data, function (error) {
               if(error)
                 console.log(error);});
@@ -93,6 +73,16 @@ function replaceTemplateTags() {
     });
   });
 }
-replaceTemplateTags();
 
+(async () => {
+  try {
+    await fsPromise.rm(target, { recursive: true, force: true });
+    await fsPromise.mkdir(target, { recursive: true });
+    copyFloader(mainDir, targetDir);
+    copyCss();
+    replaceTemplateTags();
+  } catch (err) {
+    console.log(err);
+  }
+})();
 
